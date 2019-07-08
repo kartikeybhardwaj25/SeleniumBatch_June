9c3bc65282820a6b86b5e2f40ef78fa7441fc7fb
kartikeybhardwaj25 <38663572+kartikeybhardwaj25@users.noreply.github.com>
2019-07-08 18:49:25 +0530

    committed

---
+++ TestNG_Example1/src/testng/Demo1.java
@@ -0,0 +1,20 @@
+package testng;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+  @Test
+  public void f() {
+	  System.out.println("abc");
+  }
+  
+  @Test
+  public void f1() {
+	  System.out.println("abc1");
+  }
+  
+  @Test
+  public void f3() {
+	  System.out.println("abc2");
+  }
+}
